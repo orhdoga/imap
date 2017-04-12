9de9286260e296905276b7849b503491b6489fbc
Vincent Swarte <vincent@vinsert.nl>
2017-04-12 17:31:31 +0200

    Added docblock comments

--- src/Connection.php
+++ src/Connection.php
@@ -11,21 +11,41 @@ class Connection
      */
     protected $username, $password;
 
+    /**
+     * Sets the username used for authentication by the Connection
+     *
+     * @param string $username the username
+     */
     public function setUsername(string $username)
     {
         $this->username = $username;
     }
 
+    /**
+     * Returns the username specified for authentication
+     *
+     * @return string
+     */
     public function getUsername()
     {
         return $this->username;
     }
 
+    /**
+     * Sets the password used for authentication by the Connection
+     *
+     * @param string $password the password
+     */
     public function setPassword(string $password)
     {
         $this->password = $password;
     }
 
+    /**
+     * Returns the password specified for authentication
+     *
+     * @return string
+     */
     public function getPassword()
     {
         return $this->password;
